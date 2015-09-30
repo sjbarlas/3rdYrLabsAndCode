24de72571c7f6cc446596d3651aa1d72a87bd25c
sjbarlas <c12809080@mydit.ie>
2015-09-30 12:36:28 +0100

    more

---
+++ Databases2/Oracle/create_user.sql
@@ -0,0 +1,19 @@
+-- Create a user with username patricia and password obyrne
+--
+select username, created  from DBa_users where default_tablespace LIKE 'DT2283' ORDER BY USERNAME;
+create user &&USERNAME
+identified by &PASSWORD
+default tablespace &&tbsp
+quota unlimited on &&tbsp;
+GRANT CREATE SESSION,
+      CREATE TABLE, 
+      CREATE VIEW,
+      CREATE procedure,
+      CREATE SEQUENCE,
+      CREATE TRIGGER to &&username;
+undefine password;
+undefine tbsp;
+undefine username;
+--alter user sbarlas identified by Saadat123;
+
+--select * from dba_users;
